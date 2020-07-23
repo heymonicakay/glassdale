7c26d154badb5cf07e0d5913984301ecde164f40
heymonicakay <hey.monicakay@gmail.com>
2020-07-23 12:21:26 -0500

    hashtag mug shotz

--- scripts/criminals/CriminalHTMLConverter.js
+++ scripts/criminals/CriminalHTMLConverter.js
@@ -1,11 +1,17 @@
 export const criminalHTMLConverter = (mugShot) => {
       return `
-            <section>
-            <h4 class="criminal--name">${mugShot.name}</h4>
-            <div class="criminal--specs">
-                  <p class="criminal--age">${mugShot.age}</p>
-                  <p class="criminal--crime">${mugShot.conviction}</p>
-                  <p class="criminal--term-start">${new Date(mugShot.incarceration.start).toLocaleDateString('en-US')}</p>
-                  <p class="criminal--term-end">${new Date(mugShot.incarceration.end).toLocaleDateString('en-US')}</p>
+            <section class="criminal--card">
+                  <div class="criminal--info">
+                        <p class="criminal--name">
+                              ${mugShot.name}
+                        </p>
+                        <p class="criminal--specs">
+                              Age: ${mugShot.age}<br>
+                              <span class="crime">Crime: ${mugShot.conviction}</span><br>
+                              Term start: ${new Date(mugShot.incarceration.start).toLocaleDateString('en-US')}<br>
+                              Term end: ${new Date(mugShot.incarceration.end).toLocaleDateString('en-US')}
+                        </p>
+                  </div>
+            </section>
       `
 }
